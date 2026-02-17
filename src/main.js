9dcbd35a4554da2fbd82989a5be13bf8173b117b
Oleh Musii <oleg.musiy321@gmail.com>
2026-02-17 21:37:37 +0200

    Update work

--- src/main.js
+++ src/main.js
@@ -1,6 +1,56 @@
-import { getImagesByQuery } from './pixabay-api.js';
-
-import { createGallery, clearGallery, showLoader, hideLoader } from './render-functions.js';
-
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader
+} from './js/render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const searchQuery = event.target.elements['search-text'].value.trim();
+
+  if (!searchQuery) {
+    iziToast.error({
+      message: 'Please enter a search query!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(searchQuery)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(error => {
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+        position: 'topRight',
+      });
+      console.error(error);
+    })
+    .finally(() => {
+      hideLoader();
+    });
+
+  form.reset();
+}
